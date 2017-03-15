5d6ae830abca14762fa98d5e889ac9eb1c1c6db3
travis-ci <travis@travis-ci.org>
2017-03-15 19:45:42 +0000

    ApiGen (Travis Build : 41  - Branch : mcaskill-develop)

---
+++ apigen/mcaskill-develop/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Charcoal\\Email\\Email"],["c","Charcoal\\Email\\EmailAwareTrait"],["c","Charcoal\\Email\\EmailConfig"],["c","Charcoal\\Email\\EmailInterface"],["c","Charcoal\\Email\\EmailLog"],["c","Charcoal\\Email\\EmailQueueItem"],["c","Charcoal\\Email\\EmailQueueManager"],["c","Charcoal\\Email\\GenericEmailTemplate"],["c","Charcoal\\Email\\Script\\ProcessQueueScript"],["c","Charcoal\\Email\\ServiceProvider\\EmailServiceProvider"]];
